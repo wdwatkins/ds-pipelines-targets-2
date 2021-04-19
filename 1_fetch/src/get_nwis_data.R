6a9f8691630ad38a4d34d551003275718d8c7d4a
Platt <lplatt@usgs.gov>
2021-04-19 11:54:33 -0500

    replace scipiper contents with targets

--- 1_fetch/src/get_nwis_data.R
+++ 1_fetch/src/get_nwis_data.R
@@ -21,6 +21,7 @@ nwis_site_info <- function(fileout, site_data){
   site_no <- unique(site_data$site_no)
   site_info <- dataRetrieval::readNWISsite(site_no)
   write_csv(site_info, fileout)
+  return(fileout)
 }
 
 
